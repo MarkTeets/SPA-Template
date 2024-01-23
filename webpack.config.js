89ce0681491341a4d5f51bd7ecdefa427b60338a
Mark Teets <markteets@gmail.com>
2024-01-22 20:26:16 -0700

    Initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.tsx',
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+
+  mode: process.env.NODE_ENV,
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/client/index.html'
+    })
+  ],
+
+  devServer: {
+    // Required for Docker to work with dev server
+    host: '0.0.0.0',
+    // host: 'localhost',
+    port: 8080,
+    //enable HMR on the devServer
+    hot: true,
+    // for react router
+    historyApiFallback: true,
+
+    static: {
+      // match the output path
+      directory: path.join(__dirname, '/dist'),
+      //match the output 'publicPath'
+      publicPath: '/'
+    },
+
+    headers: { 'Access-Control-Allow-Origin': '*' },
+
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000/',
+        secure: false
+      },
+      '/assets/**': {
+        target: 'http://localhost:3000/',
+        secure: false
+      }
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+        type: 'javascript/auto'
+      },
+      {
+        test: /.(css|s[ac]ss)$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
+};
